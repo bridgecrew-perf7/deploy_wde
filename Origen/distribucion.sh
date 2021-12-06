445193b12bc73ada2144888e823eb5e436852728
Juan Emilio Maydup <juanemilio.maydup@gmail.com>
2021-12-06 20:26:11 -0300

    refactor(origen validado): :art:

--- Origen/distribucion.sh
+++ Origen/distribucion.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
+dates=$(date +'%Y-%m-%d--%H%M%S')
 log=./log/log-$(date +'%Y-%m-%d--%H%M%S').log 
 errlog=./log/errlog-$(date +'%Y-%m-%d--%H%M%S').log
-echo -e $(date +'%Y-%m-%d--%H%M%S') "#############################################################################" 
-echo -e $(date +'%Y-%m-%d--%H%M%S') "#                                                                           #" 
-echo -e $(date +'%Y-%m-%d--%H%M%S') "#                 D  E  P  L  O  Y      W  D  E                             #"  
-echo -e $(date +'%Y-%m-%d--%H%M%S') "#                                                                           #" 
-echo -e $(date +'%Y-%m-%d--%H%M%S') "#############################################################################"  
-echo -e $(date +'%Y-%m-%d--%H%M%S') "# Desarrollado por: Juan Emilio Maydup            (TM) $dates #" 
-echo -e $(date +'%Y-%m-%d--%H%M%S') "#############################################################################"
+echo -e $(date +'%Y-%m-%d--%H%M%S') "#############################################################################" >> $log 
+echo -e $(date +'%Y-%m-%d--%H%M%S') "#                                                                           #" >> $log
+echo -e $(date +'%Y-%m-%d--%H%M%S') "#                 D  E  P  L  O  Y      W  D  E                             #"  >> $log
+echo -e $(date +'%Y-%m-%d--%H%M%S') "#                                                                           #" >> $log
+echo -e $(date +'%Y-%m-%d--%H%M%S') "#############################################################################"  >> $log
+echo -e $(date +'%Y-%m-%d--%H%M%S') "# Desarrollado por: Juan Emilio Maydup            (TM) $dates #" >> $log
+echo -e $(date +'%Y-%m-%d--%H%M%S') "#############################################################################" >> $log
+
 if [[ $# -eq 0 ]] ; then
 {
-    echo $(date +'%Y-%m-%d--%H%M%S') 'debe ingresar el nombre del archivo' >> $errlog
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "#############################################################################" >> $errlog
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "#                                                                           #" >> $errlog  
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "#                 D  E  P  L  O  Y      W  D  E                             #"  >> $errlog
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "#                                                                           #" >> $errlog
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "#############################################################################"  >> $errlog 
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "# Desarrollado por: Juan Emilio Maydup            (TM) $dates #" >> $errlog
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "#############################################################################" >> $errlog
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "#     debe ingresar el nombre del archivo                                     #" >> $errlog 
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "#############################################################################" >> $errlog
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "#                                                                           #" >> $errlog
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "#       .-.-------.__  __ __                                _.-^-._    .--. #"  >> $errlog
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "#      (_,         .-:-  -  --._                         .--   _   --. |__| #" >> $errlog
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "#    ,-o-(        (_,           )                       /     |_|   \\|    | #" >> $errlog  
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "#   (__,-       ,-o-(            )>                    /             \\    | #"  >> $errlog
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "#      (       (__,-             )                    /|     _____    \\   | #"  >> $errlog
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "#       ---._.--._(             )                      |    |==|==|    |  | #"  >> $errlog
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "#          |||  |||---._.--._.--        |--|--|--|--|--|    |--|--|    |  | #"  >> $errlog
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "# ^^^^^^^^^^          |||  |||          |--|--|--|--|--|    |==|==|    |  | #"  >> $errlog
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#"  >> $errlog
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "#############################################################################"  >> $errlog
     exit 1
 }
 else 
@@ -23,32 +44,51 @@ else
         for host in ${lista//,/ }
         do
             START=$(date +%s) 
-            #/usr/bin/sudo /usr/local/seguridad/bin/fttput $host $file /var/www/html/$file
+            /usr/bin/sudo /usr/local/seguridad/bin/fttput $host $file /var/www/html/$file
             END=$(date +%s) 
-            vtime=$((END-START)) | awk '{print int($1/60)":"int($1%60)}'
-            echo $(date +'%Y-%m-%d--%H%M%S') "Copiado en $host con una duracion de $vtime minutos" >> $log
+            vtime=$((END-START))
+            echo -e $(date +'%Y-%m-%d--%H%M%S') "# Copiado al $host en duracion: $(($vtime / 3600 )) horas $((($vtime % 3600) / 60)) minutos $(($vtime % 60)) segundos #" >> $log
         done 
-        GEND=$(date +%s);
-        Gvtime=$((END-START)) | awk '{print int($1/60)":"int($1%60)}'
-        echo $(date +'%Y-%m-%d--%H%M%S') "Duracion total de copiado $Gvtime minutos" >> $log 
+        GEND=$(date +%s)
+        Gvtime=$((GEND-GSTART)) 
+        echo -e $(date +'%Y-%m-%d--%H%M%S') "# Copiado a todos los host, Duracion Total: $(($Gvtime / 3600 )) horas $((($Gvtime % 3600) / 60)) minutos $(($Gvtime % 60)) segundos #" >> $log
     }
     else
     {
-        echo $(date +'%Y-%m-%d--%H%M%S') "el archivo no existe" >> $errlog
+        echo -e $(date +'%Y-%m-%d--%H%M%S') "#############################################################################" >> $errlog
+        echo -e $(date +'%Y-%m-%d--%H%M%S') "#                                                                           #" >> $errlog  
+        echo -e $(date +'%Y-%m-%d--%H%M%S') "#                 D  E  P  L  O  Y      W  D  E                             #"  >> $errlog
+        echo -e $(date +'%Y-%m-%d--%H%M%S') "#                                                                           #" >> $errlog
+        echo -e $(date +'%Y-%m-%d--%H%M%S') "#############################################################################"  >> $errlog 
+        echo -e $(date +'%Y-%m-%d--%H%M%S') "# Desarrollado por: Juan Emilio Maydup            (TM) $dates #" >> $errlog
+        echo -e $(date +'%Y-%m-%d--%H%M%S') "#############################################################################" >> $errlog
+        echo -e  $(date +'%Y-%m-%d--%H%M%S') "# el archivo no existe.                                                    #" >> $errlog
+        echo -e $(date +'%Y-%m-%d--%H%M%S') "#############################################################################" >> $errlog
+        echo -e $(date +'%Y-%m-%d--%H%M%S') "#                                                                           #" >> $errlog
+        echo -e $(date +'%Y-%m-%d--%H%M%S') "#       .-.-------.__  __ __                                _.-^-._    .--. #"  >> $errlog
+        echo -e $(date +'%Y-%m-%d--%H%M%S') "#      (_,         .-:-  -  --._                         .--   _   --. |__| #" >> $errlog
+        echo -e $(date +'%Y-%m-%d--%H%M%S') "#    ,-o-(        (_,           )                       /     |_|   \\|    | #" >> $errlog  
+        echo -e $(date +'%Y-%m-%d--%H%M%S') "#   (__,-       ,-o-(            )>                    /             \\    | #"  >> $errlog
+        echo -e $(date +'%Y-%m-%d--%H%M%S') "#      (       (__,-             )                    /|     _____    \\   | #"  >> $errlog
+        echo -e $(date +'%Y-%m-%d--%H%M%S') "#       ---._.--._(             )                      |    |==|==|    |  | #"  >> $errlog
+        echo -e $(date +'%Y-%m-%d--%H%M%S') "#          |||  |||---._.--._.--        |--|--|--|--|--|    |--|--|    |  | #"  >> $errlog
+        echo -e $(date +'%Y-%m-%d--%H%M%S') "# ^^^^^^^^^^          |||  |||          |--|--|--|--|--|    |==|==|    |  | #"  >> $errlog
+        echo -e $(date +'%Y-%m-%d--%H%M%S') "#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#"  >> $errlog
+        echo -e $(date +'%Y-%m-%d--%H%M%S') "#############################################################################"  >> $errlog
         exit 2
     }
     fi
-    echo -e $(date +'%Y-%m-%d--%H%M%S') "#############################################################################" 
-    echo -e $(date +'%Y-%m-%d--%H%M%S') "#                                                                           #" 
-    echo -e $(date +'%Y-%m-%d--%H%M%S') "#       .-.-------.__  __ __                                _.-^-._    .--. #"  
-    echo -e $(date +'%Y-%m-%d--%H%M%S') "#      (_,         .-:-  -  --._                         .--   _   --. |__| #"  
-    echo -e $(date +'%Y-%m-%d--%H%M%S') "#    ,-o-(        (_,           )                       /     |_|   \\|    | #"  
-    echo -e $(date +'%Y-%m-%d--%H%M%S') "#   (__,-       ,-o-(            )>                    /             \\    | #"  
-    echo -e $(date +'%Y-%m-%d--%H%M%S') "#      (       (__,-             )                    /|     _____    \\   | #"  
-    echo -e $(date +'%Y-%m-%d--%H%M%S') "#       ---._.--._(             )                      |    |==|==|    |  | #"  
-    echo -e $(date +'%Y-%m-%d--%H%M%S') "#          |||  |||---._.--._.--        |--|--|--|--|--|    |--|--|    |  | #"  
-    echo -e $(date +'%Y-%m-%d--%H%M%S') "# ^^^^^^^^^^          |||  |||          |--|--|--|--|--|    |==|==|    |  | #"  
-    echo -e $(date +'%Y-%m-%d--%H%M%S') "#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#"  
-    echo -e $(date +'%Y-%m-%d--%H%M%S') "#############################################################################"  
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "#############################################################################" >> $log
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "#                                                                           #" >> $log
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "#       .-.-------.__  __ __                                _.-^-._    .--. #"  >> $log
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "#      (_,         .-:-  -  --._                         .--   _   --. |__| #" >> $log
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "#    ,-o-(        (_,           )                       /     |_|   \\|    | #" >> $log  
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "#   (__,-       ,-o-(            )>                    /             \\    | #"  >> $log
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "#      (       (__,-             )                    /|     _____    \\   | #"  >> $log
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "#       ---._.--._(             )                      |    |==|==|    |  | #"  >> $log
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "#          |||  |||---._.--._.--        |--|--|--|--|--|    |--|--|    |  | #"  >> $log
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "# ^^^^^^^^^^          |||  |||          |--|--|--|--|--|    |==|==|    |  | #"  >> $log
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#"  >> $log
+    echo -e $(date +'%Y-%m-%d--%H%M%S') "#############################################################################"  >> $log
 }
 fi
